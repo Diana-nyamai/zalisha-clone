499dc0b8904f1c7a0f498fa83983df882438f3e8
Diana-nyamai <dnyamai.dn@gmail.com>
2021-09-30 15:18:10 +0300

    update

---
+++ trial/trial.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "1000px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "900px";
+  }
