52c3286ec5f2f600ca4ed66e9525f887555e2f48
Diana-nyamai <dnyamai.dn@gmail.com>
2021-10-05 17:17:02 +0300

    update

--- index.js
+++ index.js
@@ -16,3 +16,14 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  // carousel script
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    loop: true
+  });
